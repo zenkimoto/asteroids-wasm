5161d3cbcb3e458c950e6f4ddb0d5d34348b7ce1
Alex Yip <alexyip123@gmail.com>
2020-06-22 06:25:40 -0700

    Setup quicksilver window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use quicksilver::{
+    run, Graphics, Input, Result, Settings, Window,
+};
+
 fn main() {
-    println!("Hello, world!");
+    run(
+        Settings {
+            title: "Asteroids",
+            ..Settings::default()
+        },
+        app,
+    );
+}
+
+async fn app(window: Window, gfx: Graphics, mut input: Input) -> Result<()> {
+    let window_size = window.size();
+
+    println!("Window Size: {:?}", window_size);  // Default: 1024.0 x 768.0
+
+    loop {
+        while let Some(_) = input.next_event().await {
+            // Normally we'd do some processing here
+        }
+        // And then we'd do updates and drawing here
+        // When this loop ends, the window will close and the application will stop
+        // If the window is closed, our application will receive a close event and terminate also
+    }
 }
