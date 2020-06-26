8777f2efcdfbef265ab8cdac71cbc7bf382f6271
Alex Yip <alexyip123@gmail.com>
2020-06-25 21:32:44 -0700

    Refactored main

--- src/main.rs
+++ src/main.rs
@@ -30,22 +30,36 @@ async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> 
     let mut state = GameState::new(&window_size);
 
     loop {
-        while let Some(e) = input.next_event().await {
-            match e {
-                Event::KeyboardInput(key) if key.is_down() => state.key_down(key.key()),
-                Event::KeyboardInput(key) if key.is_down() == false => state.key_up(key.key()),
-                _ => { }
-            }
-         }
+        handle_input_events(&mut input, &mut state).await;
 
-        // We use a while loop rather than an if so that we can try to catch up in the event of having a slow down.
-        while update_timer.tick() {
-            state.update(&mut input);
-        }
+        update_game_state(&mut update_timer, &mut input, &mut state);
 
-        if draw_timer.exhaust().is_some() {
-            state.render(&mut gfx)?;
-            gfx.present(&window)?;
-        }
+        render_game_state(&mut draw_timer, &window, &mut gfx, &mut state)?;
     }
+}
+
+async fn handle_input_events(input: &mut Input, state: &mut GameState) {
+    while let Some(e) = input.next_event().await {
+        match e {
+            Event::KeyboardInput(key) if key.is_down() => state.key_down(key.key()),
+            Event::KeyboardInput(key) if key.is_down() == false => state.key_up(key.key()),
+            _ => { }
+        }
+     }
+}
+
+fn update_game_state(update_timer: &mut Timer, input: &mut Input, state: &mut GameState) {
+    // We use a while loop rather than an if so that we can try to catch up in the event of having a slow down.
+    while update_timer.tick() {
+        state.update(input);
+    }
+}
+
+fn render_game_state(draw_timer: &mut Timer, window: &Window, gfx: &mut Graphics, state: &mut GameState) -> Result<()> {
+    if draw_timer.exhaust().is_some() {
+        state.render(gfx)?;
+        gfx.present(window)?;
+    }
+
+    Ok(())
 }
