53ddebe9bb93777e42845962fe7ec8bf7471f30e
Alex Yip <alexyip123@gmail.com>
2020-06-27 05:18:22 -0700

    Added state stack

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,10 @@ fn main() {
     );
 }
 
+enum StateType {
+    InitialState(GameState),
+}
+
 async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
    let window_size = window.size();
 
@@ -27,18 +31,28 @@ async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> 
     let mut update_timer = Timer::time_per_second(30.0);
     let mut draw_timer = Timer::time_per_second(60.0);
 
-    let mut state = GameState::new(&window_size);
+    let mut states = vec![
+        StateType::InitialState(GameState::new(&window_size))
+    ];
 
     loop {
-        handle_input_events(&mut input, &mut state).await;
+        if let Some(state_type) = states.last_mut() {
+            let state = match state_type {
+                StateType::InitialState(state) => state,
+            };
 
-        update_game_state(&mut update_timer, &mut input, &mut state);
+            handle_input_events(&mut input, state).await;
 
-        render_game_state(&mut draw_timer, &window, &mut gfx, &mut state)?;
+            update_game_state(&mut update_timer, &mut input, state);
+
+            render_game_state(&mut draw_timer, &window, &mut gfx, state)?;
+        } else {
+            panic!("No states in state stack!");
+        }
     }
 }
 
-async fn handle_input_events(input: &mut Input, state: &mut GameState) {
+async fn handle_input_events(input: &mut Input, state: &mut dyn State) {
     while let Some(e) = input.next_event().await {
         match e {
             Event::KeyboardInput(key) if key.is_down() => state.key_down(key.key()),
@@ -48,14 +62,14 @@ async fn handle_input_events(input: &mut Input, state: &mut GameState) {
      }
 }
 
-fn update_game_state(update_timer: &mut Timer, input: &mut Input, state: &mut GameState) {
+fn update_game_state(update_timer: &mut Timer, input: &mut Input, state: &mut dyn State) {
     // We use a while loop rather than an if so that we can try to catch up in the event of having a slow down.
     while update_timer.tick() {
         state.update(input);
     }
 }
 
-fn render_game_state(draw_timer: &mut Timer, window: &Window, gfx: &mut Graphics, state: &mut GameState) -> Result<()> {
+fn render_game_state(draw_timer: &mut Timer, window: &Window, gfx: &mut Graphics, state: &mut dyn State) -> Result<()> {
     if draw_timer.exhaust().is_some() {
         state.render(gfx)?;
         gfx.present(window)?;
