d0791e06771d5e94b22377c1f118f3b28f6bd332
Alex Yip <alexyip123@gmail.com>
2020-07-10 08:27:06 -0700

    Updated deployment script to launch basic http server

--- webdeploy.sh
+++ webdeploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 cargo web deploy --features "quicksilver/stdweb getrandom/stdweb"
+
+# Install basic http server to use
+# https://github.com/brson/basic-http-server
+basic-http-server target/deploy
